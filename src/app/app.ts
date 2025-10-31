8805bb077fc1408fd37adbe238cf66cd79364393
lucas <lucashamacher01@gmail.com>
2025-10-31 14:24:30 +0100

    implement component routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,12 @@ import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref, RouterLink } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIcon, MatSidenavModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIcon, MatSidenavModule, RouterLinkWithHref, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
